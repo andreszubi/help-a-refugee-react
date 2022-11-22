03179fb7c54a1584e9329792bb10cba793234259
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-22 18:32:12 +0100

    remove unnecessary text

--- help-a-refugee/src/main.jsx
+++ help-a-refugee/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider} from '@mantine/core';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
