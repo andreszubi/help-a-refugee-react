6ac6d6b0083cff091ff7b774168145dc386896fe
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-23 13:54:56 +0100

    setting up home and some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,26 +13,33 @@ function Home() {
 
       <NavBar />
      
-
-      <h1>Home</h1>
-
-      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nibh lorem, egestas aliquet egestas eu, porta ac magna. Donec luctus ligula nec dolor lobortis, eget pharetra ipsum efficitur. Aliquam tincidunt magna quis nunc consequat bibendum. Cras semper orci metus, in tempor tortor aliquet id. Nullam ullamcorper nisl non tempus viverra. Fusce vitae aliquam sem. Vivamus a metus in sem porta aliquam. Nam blandit lectus lorem, a maximus nunc pharetra ac. Nullam quis magna non ligula scelerisque efficitur. Integer at enim id diam rutrum suscipit vel non elit. Praesent elementum auctor leo eget egestas.</h2>
-
+       <div className="background-img">
+      
+  <div class="transbox">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at libero nec risus tincidunt luctus. Nulla tempus eu massa sed efficitur. Proin interdum quis velit blandit vulputate. Duis tempus vehicula augue, eget tincidunt lectus. Ut vel sapien ut diam volutpat volutpat. In aliquet lectus eu massa pulvinar, quis hendrerit elit congue. Nulla augue nunc, consequat a mattis id, egestas in ligula. Pellentesque sodales dignissim dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent nec nisl in libero mollis vehicula. Pellentesque pharetra metus posuere nunc euismod sodales. Donec eleifend nibh urna, ac lacinia quam suscipit quis. In hac habitasse platea dictumst. Aliquam auctor augue lobortis enim accumsan sodales.
+Nam sollicitudin, augue in posuere luctus, urna lorem placerat ex, porta placerat tellus sem ut justo. In egestas, magna sed maximus volutpat, ipsum eros mattis sapien, id varius erat libero vel metus. Mauris efficitur sem sed porttitor eleifend. Curabitur id nulla ac turpis pellentesque consectetur placerat sit amet sapien. Morbi porttitor, urna sit amet rhoncus auctor, ipsum diam molestie nulla, vitae commodo nunc leo non sem. Pellentesque euismod, ligula in finibus molestie, felis ipsum lacinia lacus, et aliquam nisl odio eget arcu. Ut feugiat eget diam nec faucibus. Sed convallis, metus eu lobortis lacinia, orci arcu tempus justo, vulputate malesuada metus dui ac ex. Quisque mattis, sapien sit amet lacinia finibus, leo diam fermentum augue, efficitur consectetur eros tellus in felis. Fusce enim lorem, tincidunt non nisl vel, tincidunt molestie justo. Curabitur luctus ipsum commodo tellus auctor, id fringilla tellus commodo.
+Vivamus at dolor neque. Donec blandit iaculis elit, nec fermentum nisl imperdiet ut. Morbi id vulputate lectus, a iaculis arcu. Curabitur eu vulputate velit. Praesent hendrerit felis ut ex facilisis laoreet. Etiam imperdiet sagittis sapien, eu ultrices neque euismod sodales. Quisque maximus aliquam felis quis volutpat. Sed sit amet magna sed lectus sagittis tincidunt sollicitudin nec risus. Suspendisse tincidunt.</p>
+ 
+</div>
       <form>
         <label>Email<input type="email" name="email" /></label>
         <label>Password<input type="password" name="password" /></label>
-        <button type="button">Login</button>
+        <button className="button" type="button">Login</button>
       </form>
+
+     
 
 
       
+       <div className="signup-btns">
+      <Link to="/host-signup"><button className="button"  type="button">Host Signup</button></Link>
+      
+      <Link to="/refugee-signup"><button className="button" type="button">Refugee Signup</button></Link>
+      </div>
 
-      <Link to="/host-signup"><button type="button">Host Signup</button></Link>
-      <Link to="/refugee-signup"><button type="button">Refugee Signup</button></Link>
 
 
-
-
+      </div>
 
       
 
