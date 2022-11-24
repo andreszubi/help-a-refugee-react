215690fc56a3acc5547886e575d1a7d4c0407fc4
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-24 14:42:16 +0100

    added publish listing form

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import HostLogin from "./pages/Host-Login";
 // import RefugeeProfile from './components/Refugee-Profile'
 // import HostProfile from './components/Host-Profile'
 // import ListingsSearch from './components/Listings-Search'
-// import PublishListing from './components/Publish-Listing'
+import PublishListing from "./pages/Publish-Listing"
 // import ListingDetails from './components/Listing-Details'
 // import Messages from './components/Messages'
 
@@ -22,10 +22,10 @@ function App() {
         <Route path="/host-login" element={<HostLogin />} />
         {/* <Route path="/refugee-profile" element={<RefugeeProfile />} />
         <Route path="/host-profile" element={<HostProfile />} />
-        <Route path="/listings-search" element={<ListingsSearch />} />
+        <Route path="/listings-search" element={<ListingsSearch />} /> */}
         <Route path="/publish-listing" element={<PublishListing />} />
-        <Route path="/listing-details" element={<ListingDetails />} />
-        <Route path="/messages" element={<Messages />} /> */}
+        {/* <Route path="/listing-details" element={<ListingDetails />} /> */}
+        {/* <Route path="/messages" element={<Messages />} /> */}
       </Routes>
     </div>
   );
