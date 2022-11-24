c695df8fdabc870c3cb06bee26cf553d031f40ae
Tatiana <taty07@gmail.com>
2022-11-24 16:10:35 +0100

    bla

--- src/pages/User-Profile.jsx
+++ src/pages/User-Profile.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
 import NavBarUser from "../components/NavBarUser";
 import { SessionContextUser } from "../contexts/SessionContextUser";
-import { NavLink } from "react-router-dom";
 
 const UserProfilePage = () => {
-  const { isAuthenticated } = useContext(SessionContextUser);
+  const { user } = useContext(SessionContextUser);
+
+  // useEffect(() => {}, []);
+  // if (currentToken) {
+  //     console.log(currentToken);
+  // }
+
+  //   const [email, setEmail] = useState();
+  //   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <>
+    <div>
       <NavBarUser />
-      <div>ProfilePage</div>
-    </>
+
+      <div>Profile Page</div>
+      <h3>user.firstName</h3>
+      <h3>user.lastName</h3>
+      <h3>user.email</h3>
+      <h3>user.aboutMe</h3>
+    </div>
   );
 };
 
