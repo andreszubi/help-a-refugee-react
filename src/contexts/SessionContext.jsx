cbe4f759ccc05ca285393cc80fab6710d87205ef
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-24 08:52:43 +0000

    session context file

---
+++ src/contexts/SessionContext.jsx
@@ -0,0 +1,52 @@
+import { useLocalStorage } from "@mantine/hooks";
+import { createContext, useEffect, useState } from "react";
+
+export const SessionContext = createContext();
+
+const SessionContextProvider = ({children}) => {
+    const [token, setToken] = useLocalStorage({key: "token", defaultValue: undefined})
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    const verifyToken = async () => {
+        const response = await fetch("http://localhost:5005/host/verify", {
+            headers:{
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        const parsed = await response.json()
+        console.log(parsed.payload)
+        if (parsed.message === "Token OK") {
+            setIsAuthenticated(true)
+        }
+    }
+    useEffect (()=> {
+        if (token) {
+            verifyToken()
+        } else {
+            setIsAuthenticated(false)
+        }
+    }, [token])
+
+    function fetchWithToken (method, endpoint, callback, body = null) {
+        async () => {
+            const response = await fetch(`http://localhost:5005/${endpoint}`, {
+                method,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type" : "application/json",
+                },
+                body,
+            })
+            const parsed = await response.json()
+            callback(parsed)
+        }
+    }
+
+    return (
+        <SessionContext.Provider value={{token, setToken, isAuthenticated, fetchWithToken}}>
+            {children}
+        </SessionContext.Provider>
+    )
+}
+
+export default SessionContextProvider
