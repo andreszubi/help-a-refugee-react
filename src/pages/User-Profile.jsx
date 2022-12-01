d56bdeee112fd61ab8b3026dce4be5fa450bd1ea
Tatiana <taty07@gmail.com>
2022-12-01 12:11:11 +0100

    css done

--- src/pages/User-Profile.jsx
+++ src/pages/User-Profile.jsx
@@ -81,103 +81,105 @@ const UserProfile = () => {
   }, [currentUser]);
 
   return (
-    <div className="container">
-      <NavBarUser place={place} />
-      {isLoading ? (
-        <div>
-          <Loader color="grape" />
-        </div>
-      ) : (
-        <div>
-          <div className="font-link largeTextSignUp">
-            <h1>Welcome to your profile, {currentUser.user.firstName}</h1>{" "}
+    <div>
+      <div className="container">
+        <NavBarUser place={place} />
+        {isLoading ? (
+          <div>
+            <Loader color="grape" />
           </div>
-
-          <Skeleton visible={isLoading}>
-            <div className="profileFull">
-              <Card shadow="sm" p="lg" radius="md" withBorder>
-                <div className="profileInfo">
-                  <img
-                    className="profileImage"
-                    src={currentUser.user.image}
-                    alt="user photo"
-                  />
-                  <div className="allProfileText">
-                    <Text className="profileText">
-                      First Name: {currentUser.user.firstName}
-                    </Text>
-                    <Text className="profileText">
-                      Last Name: {currentUser.user.lastName}
-                    </Text>
-                    <Text className="profileText">
-                      Email: {currentUser.user.email}
-                    </Text>
-                    <Text className="profileText">
-                      About me:
-                      {currentUser.user.aboutMe}
-                    </Text>
-                    <button
-                      className="button profButton"
-                      type="submit"
-                      onClick={() => setIsEditing(true)}
-                    >
-                      Edit profile{" "}
-                    </button>
-                  </div>
-                </div>
-              </Card>
+        ) : (
+          <div>
+            <div className="font-link largeTextSignUp">
+              <h1>Welcome to your profile, {currentUser.user.firstName}</h1>{" "}
             </div>
-          </Skeleton>
 
-          <Modal
-            opened={isEditing}
-            onClose={() => setIsEditing(false)}
-            title="Edit profile"
-          >
-            <form onSubmit={handleSubmit} className="formEdit">
-              <label>
-                Profile Picture:{" "}
-                <input
-                  type="file"
-                  name="imageUrl"
-                  accept="image/png, image/jpg"
-                />
-              </label>
-              <label>
-                First Name :
-                <input
-                  value={firstName}
-                  onChange={(event) => setFirstName(event.target.value)}
-                />
-              </label>
-              <label>
-                Last Name :
-                <input
-                  value={lastName}
-                  onChange={(event) => setLastName(event.target.value)}
-                />
-              </label>
-              <label>
-                Email:
-                <input
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                />
-              </label>
-              <label>
-                About me:
-                <input
-                  value={aboutMe}
-                  onChange={(event) => setAboutMe(event.target.value)}
-                />
-              </label>
-              <button className="button" type="submit">
-                Update
-              </button>
-            </form>
-          </Modal>
-        </div>
-      )}
+            <Skeleton visible={isLoading}>
+              <div className="profileFull">
+                <Card shadow="sm" p="lg" radius="md" withBorder>
+                  <div className="profileInfo">
+                    <img
+                      className="profileImage"
+                      src={currentUser.user.image}
+                      alt="user photo"
+                    />
+                    <div className="allProfileText">
+                      <Text className="profileText">
+                        First Name: {currentUser.user.firstName}
+                      </Text>
+                      <Text className="profileText">
+                        Last Name: {currentUser.user.lastName}
+                      </Text>
+                      <Text className="profileText">
+                        Email: {currentUser.user.email}
+                      </Text>
+                      <Text className="profileText">
+                        About me:
+                        {currentUser.user.aboutMe}
+                      </Text>
+                      <button
+                        className="button profButton"
+                        type="submit"
+                        onClick={() => setIsEditing(true)}
+                      >
+                        Edit profile{" "}
+                      </button>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </Skeleton>
+
+            <Modal
+              opened={isEditing}
+              onClose={() => setIsEditing(false)}
+              title="Edit profile"
+            >
+              <form onSubmit={handleSubmit} className="formEdit">
+                <label>
+                  Profile Picture:{" "}
+                  <input
+                    type="file"
+                    name="imageUrl"
+                    accept="image/png, image/jpg"
+                  />
+                </label>
+                <label>
+                  First Name :
+                  <input
+                    value={firstName}
+                    onChange={(event) => setFirstName(event.target.value)}
+                  />
+                </label>
+                <label>
+                  Last Name :
+                  <input
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
+                  />
+                </label>
+                <label>
+                  Email:
+                  <input
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </label>
+                <label>
+                  About me:
+                  <input
+                    value={aboutMe}
+                    onChange={(event) => setAboutMe(event.target.value)}
+                  />
+                </label>
+                <button className="button" type="submit">
+                  Update
+                </button>
+              </form>
+            </Modal>
+          </div>
+        )}
+      </div>
       <Footer />
     </div>
   );
