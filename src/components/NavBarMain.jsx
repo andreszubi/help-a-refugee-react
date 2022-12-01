05e7566cd518239bc39f70bbc7b82522e1a0af24
Tatiana <taty07@gmail.com>
2022-12-01 10:13:17 +0100

    host done + logo

--- src/components/NavBarMain.jsx
+++ src/components/NavBarMain.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "../images/Logo2.png";
 
 function NavBarMain() {
   return (
--- src/components/NavBarUser.jsx
+++ src/components/NavBarUser.jsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
+import logo from "../images/Logo2.png";
 
 function NavBarUser({ place }) {
   const navigate = useNavigate();
